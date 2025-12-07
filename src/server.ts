69c3b418dbbcfcd2db8041db79f50be726f2fdf8
Thanaphon Sarnsamuth <thanaphon.sar@axonstech.com>
2025-12-07 10:15:53 +0700

    task 4

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+
+app.get("/test1", (req, res) => {
+  res.send("Hello World! 3");
+  let returnObj = {
+    name: "test",
+    age: 20,
+    address: "Thai",
+  };
+  res.send(returnObj);
+});
+
+app.get("/test", (req: Request, res: Response) => {
+  const id = req.query.id;
+  const output = `id: ${id}`;
+  res.send(output);
+});
