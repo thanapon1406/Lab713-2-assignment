3c002524a65720b6fe9c6474cdefae7f09342ab3
Thanaphon Sarnsamuth <thanaphon.sar@axonstech.com>
2025-12-07 10:25:06 +0700

    task5 1-4

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,80 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = 3000;
 
+interface Event {
+  id: number;
+  category: string;
+  title: string;
+  description: string;
+  location: string;
+  date: string;
+  time: string;
+  petsAllowed: boolean;
+  organizer: string;
+}
+
+const events: Event[] = [
+  {
+    id: 1,
+    category: "Music",
+    title: "Concert",
+    description: "A live concert",
+    location: "London",
+    date: "2021-07-01",
+    time: "19:00",
+    petsAllowed: false,
+    organizer: "Live Nation",
+  },
+  {
+    id: 2,
+    category: "Music",
+    title: "Concert",
+    description: "City Pop Live",
+    location: "Tokyo",
+    date: "2021-08-01",
+    time: "19:00",
+    petsAllowed: false,
+    organizer: "Live Nation",
+  },
+  {
+    id: 3,
+    category: "Pet",
+    title: "Pet Event",
+    description: "A Pet Day",
+    location: "Bangkok",
+    date: "2021-09-01",
+    time: "19:00",
+    petsAllowed: true,
+    organizer: "Live Nation",
+  },
+  {
+    id: 4,
+    category: "Music",
+    title: "Concert",
+    description: "Rock Music Live",
+    location: "Manchester",
+    date: "2021-10-01",
+    time: "19:00",
+    petsAllowed: false,
+    organizer: "Live Nation",
+  },
+  {
+    id: 5,
+    category: "Music",
+    title: "Concert",
+    description: "Jazz Live",
+    location: "Chicago",
+    date: "2021-11-01",
+    time: "19:00",
+    petsAllowed: false,
+    organizer: "Live Nation",
+  },
+];
+
+app.get("/events", (req, res) => {
+  res.json(events);
+});
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
